a16f62285201b024e09a237bc8bf49f205910bfd
Julio Cesar <julio.scheidt96@gmail.com>
2020-10-02 01:55:25 -0300

    Several improvements on services

--- python-rabbitmq/deploy.sh
+++ python-rabbitmq/deploy.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# (kubectl delete -f ./k8s 1> /dev/null 2>&1 && echo "Cleaning pods...") &
-# wait
-
-# secrets
 kubectl apply -f ./k8s/secrets.yaml
-
-# deployment and service rabbit
-kubectl apply -f ./k8s/rabbit-statefulset.yaml
-kubectl apply -f ./k8s/rabbit-cluster-ip-service.yaml
-
-# deployment consumers
-kubectl apply -f ./k8s/consumer-01-deployment.yaml
-kubectl apply -f ./k8s/consumer-02-deployment.yaml
-
-# deployment and service publisher
-kubectl apply -f ./k8s/publisher-deployment.yaml
+kubectl apply -f ./k8s/rabbit.yaml
+kubectl apply -f ./k8s/consumer01.yaml
+kubectl apply -f ./k8s/consumer02.yaml
+kubectl apply -f ./k8s/publisher.yaml
--- python-rabbitmq/rabbit/rabbitmq-config.sh
+++ python-rabbitmq/rabbit/rabbitmq-config.sh
@@ -16,17 +16,18 @@ rabbitmqctl set_permissions -p / guest ".*" ".*" ".*"
 # amqp tools
 # apt-get install amqp-tools -y
 
+# rabbitmqctl set_policy TTL ".*" '{"message-ttl": 60000}' --apply-to queues
+
 # queues
 # rabbitmqadmin -u {user} -p {password} -V {vhost} declare queue name={nanme} auto_delete=false durable=true
 rabbitmqadmin declare --vhost=/ queue name=event_queue_01 auto_delete=false durable=true
 rabbitmqadmin declare --vhost=/ queue name=event_queue_02 auto_delete=false durable=true
-# dead letter queue
-rabbitmqadmin declare --vhost=/ queue name=dead_letter auto_delete=false durable=true arguments='{"x-message-ttl": 300000, "x-dead-letter-exchange":"event_queue_exchange","x-dead-letter-routing-key":""}'
+# dead letter queue (TTL of 60 seconds)
+rabbitmqadmin declare --vhost=/ queue name=dead_letter auto_delete=false durable=true arguments='{"x-message-ttl": 60000, "x-dead-letter-exchange":"event_queue_exchange","x-dead-letter-routing-key":""}'
 
 # exchanges
 # types: direct, topic, headers and fanout
-# fanout exchange: it broadcasts all the messages it receives to all the queues it knows
-rabbitmqadmin declare exchange name=event_queue_exchange type=fanout auto_delete=false durable=true
+rabbitmqadmin declare exchange name=event_queue_exchange type=topic auto_delete=false durable=true
 
 # bindings
 rabbitmqadmin declare binding source=event_queue_exchange destination=event_queue_01 routing_key="event_queue_01.#" destination_type=queue
